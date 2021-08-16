98407234cd66740ca8d36d26b595176f4bdbe723
Li-C-N <ljd2413@qq.com>
2021-08-16 23:54:07 +0800

    common code

--- blog-blog_main/src/main/java/pers/ljd/blog/BlogServerApplication.java
+++ blog-blog_main/src/main/java/pers/ljd/blog/BlogServerApplication.java
@@ -1,4 +1,4 @@
-package pers.ljd.nacos;
+package pers.ljd.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class NacosApplication {
+public class BlogServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosApplication.class, args);
+        SpringApplication.run(BlogServerApplication.class, args);
     }
 
 }
